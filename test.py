b9d899b1ff91c0c8457fd0cd3953180573a9db40
Buck Wallander <bwalla@gmail.com>
2020-12-06 15:22:13 -0800

    intentional CI breakage test

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ from ipinput import routeLookup, openRouteTableFile, Route
 class Unit_Testing(unittest.TestCase):
     def testECMP(self):
         result = routeLookup("1.1.1.1")
-        self.assertEqual(len(result), 2)
+        self.assertEqual(len(result), 3)
 
     def testObj(self):
         result = routeLookup("1.1.1.1")
-        self.assertTrue(isinstance(result[0], Route))
+        self.assertFalse(isinstance(result[0], Route))
 
 if __name__ == '__main__':
     unittest.main()
